0e466682984c1faa3b03ec20c2a4335a17969826
shibom-lang <shibomdas80@gmail.com>
2026-08-03 20:35:06 +0530

    Remove unused jinja2 import

--- api/index.py
+++ api/index.py
@@ -1,8 +1,4 @@
-import asyncio
 from fastapi import FastAPI, Request
-from fastapi.responses import HTMLResponse
-from fastapi.staticfiles import StaticFiles
-from fastapi.templating import Jinja2Templates
 from pydantic import BaseModel
 from google.antigravity import Agent, LocalAgentConfig, types
 import os
